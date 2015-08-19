8e55fad2f796f72dfa21404bbd444b9e301d8032
庞辉 <huipang@corp.netease.com>
2015-08-19 09:36:50 +0800

    稳定版本提交云测测试；

--- LDNetDiagnoServiceDemo_Android/src/com/netease/ldnetdiagnoservicedemo_android/MainActivity.java
+++ LDNetDiagnoServiceDemo_Android/src/com/netease/ldnetdiagnoservicedemo_android/MainActivity.java
@@ -35,8 +35,9 @@ public class MainActivity extends Activity implements OnClickListener,
     progress.setVisibility(View.INVISIBLE);
     text = (TextView) findViewById(R.id.text);
     edit = (EditText) findViewById(R.id.domainName);
+    //edit，提交云测的时候，固定采用默认域名进行测试
+    edit.setInputType(InputType.TYPE_NULL);
     edit.clearFocus();
-    onClick(btn);
   }
 
   @Override
@@ -57,14 +58,14 @@ public class MainActivity extends Activity implements OnClickListener,
         text.setText("Traceroute with max 30 hops...");
         btn.setText("停止诊断");
         btn.setEnabled(false);
-        edit.setInputType(InputType.TYPE_NULL);
+//        edit.setInputType(InputType.TYPE_NULL);
       } else {
         progress.setVisibility(View.GONE);
         btn.setText("开始诊断");
         _netDiagnoService.cancel(true);
         _netDiagnoService = null;
         btn.setEnabled(true);
-        edit.setInputType(InputType.TYPE_CLASS_TEXT);
+//        edit.setInputType(InputType.TYPE_CLASS_TEXT);
       }
 
       isRunning = !isRunning;
